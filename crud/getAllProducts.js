a575f4e41603c0dfb7cca6b1530f7c10885246fe
Gabbe2003 <gabriell.touma@gmail.com>
2024-06-07 14:20:18 +0200

    paginering

--- crud/getAllProducts.js
+++ crud/getAllProducts.js
@@ -1,13 +1,24 @@
 const movies = require('../models/movies');
 
-const getAllMovies = async ( req, res ) => {
+const getAllMovies = async (req, res) => {
+    const page = parseInt(req.query.page) || 1;   
+    const limit = parseInt(req.query.limit) || 5; 
+    const skip = (page - 1) * limit; 
 
     try {
-        const allMovies = await movies.find({});
-        return res.status(200).json({ message: 'Here is the Movies', allMovies});
+        const allMovies = await movies.find({}).skip(skip).limit(limit);
+        const total = await movies.countDocuments({});
+
+        return res.status(200).json({
+            message: 'Here are the movies',
+            total,
+            page,
+            pages: Math.ceil(total / limit),
+            data: allMovies
+        });
     } catch (error) {
         console.log(error);
-        return res.status(500).json({ message: 'Internal Server Error.', error});
+        return res.status(500).json({ message: 'Internal Server Error.', error });
     }
 }
 
