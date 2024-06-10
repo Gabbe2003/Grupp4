5d11f32712c82ed6c895d19851e0e8159b0efe56
Sebbe-L <sebastian.lundberg@medieinstitutet.se>
2024-06-10 12:41:11 +0200

    map

--- routes/router.js
+++ routes/router.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const allMovies = require('../crud/getAllProducts');
-const getOneMovie = require('../crud/getOneProduct');
+const allMovies = require("../crud/getAllProducts");
+const getOneMovie = require("../crud/getOneProduct");
 
-router.get('/getAllMovies', allMovies.getAllMovies);
-router.get('/getAllMovies/:id', getOneMovie.getMovieById);
+router.get("/getAllMovies", allMovies.getAllMovies);
+router.get("/getAllMovies/:movieId", getOneMovie.getMovieById);
 
 module.exports = router;
